4c58b90664cb05a332a0fef5fe50ef1979e661da
Unathi Mathokazi <unathimathokaziu@gmail.com>
2025-07-20 23:34:14 +0200

    fix: implement and connect UserContext properly

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,19 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext } from "react";
 
 const UserContext = createContext();
-
-export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: 'Nelly',
-    email: 'nelly@example.com',
-    // any other user info
-  });
-
-  return (
-    <UserContext.Provider value={user}>
-      {children}
-    </UserContext.Provider>
-  );
-};
 
 export default UserContext;
