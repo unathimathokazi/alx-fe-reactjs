5654b777d0bd99edacb7b314605215e2a6ec622b
Unathi Mathokazi <unathimathokaziu@gmail.com>
2025-08-24 17:33:56 +0200

    Fix App.jsx and TodoList for task 3

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,12 +1,10 @@
 import TodoList from "./components/TodoList";
-import TodoList from "./components/TodoList";
 
 export default function App() {
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
       <h1>My Todo App</h1>
       <TodoList />
-      <Route path="/todos" element={<TodoList />} />
     </div>
   );
 }
