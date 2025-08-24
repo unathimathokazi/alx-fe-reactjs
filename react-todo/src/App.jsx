08729a4c169d182cccac3a94cd5cc817182a1409
Unathi Mathokazi <unathimathokaziu@gmail.com>
2025-08-24 21:49:05 +0200

    Task 3: Integrate TodoList and tests into App

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -2,7 +2,7 @@ import TodoList from "./components/TodoList";
 
 export default function App() {
   return (
-    <div style={{ padding: "2rem" }}>
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
       <h1>My Todo App</h1>
       <TodoList />
     </div>
